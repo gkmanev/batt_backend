f1c572a4b71ed1e85f72ed17ea72bb445b55896b
Georgi <georgi.manev@entra.energy>
2024-08-29 12:19:08 +0300

    init

--- battery_backed/views.py
+++ battery_backed/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class StateViewSet(viewsets.ModelViewSet):
     queryset = BatteryLiveStatus.objects.all()
     serializer_class = BatteryLiveSerializer
-    queryset = queryset.filter(state_of_charge__gte=0, state_of_charge__lte=100)
+    
 
     def get_queryset(self):
         queryset =  super().get_queryset()        
@@ -17,5 +17,11 @@ class StateViewSet(viewsets.ModelViewSet):
             today = timezone.now().date()           
             if date_range == 'today':
                 queryset = queryset.filter(timestamp__gte=today).order_by('timestamp')
+            # Adjust the state_of_charge values based on your conditions
+        for obj in queryset:
+            if obj.state_of_charge <= 0:
+                obj.state_of_charge = 0
+            elif obj.state_of_charge >= 100:
+                obj.state_of_charge = 100
 
         return queryset
